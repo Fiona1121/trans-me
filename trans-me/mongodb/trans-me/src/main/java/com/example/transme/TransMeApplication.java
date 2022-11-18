6033088ed825436ff32058fa052e553f3456b85c
Sebastian <sebastil@hotmail.com>
2022-11-18 22:20:54 +0800

    first-commit

---
+++ trans-me/mongodb/trans-me/src/main/java/com/example/transme/TransMeApplication.java
@@ -0,0 +1,13 @@
+package com.example.transme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransMeApplication.class, args);
+	}
+
+}
---
+++ trans-me/mongodb/trans-me/src/test/java/com/example/transme/TransMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.transme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
