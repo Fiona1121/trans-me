94c241f6d56f8088fc8aa9642683f63c1d838102
itzsyboo <55495526+Fiona1121@users.noreply.github.com>
2022-11-23 15:35:28 +0800

    Frontend-#6 Finish basic block operation

--- trans-me/src/containers/main/index.js
+++ trans-me/src/containers/main/index.js
@@ -1,14 +1,21 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
+import { selectGlobal } from "../../slices/globalSlice";
 import { selectSession } from "../../slices/sessionSlice";
+import Board from "./board";
 
 export default function Main() {
   const { username, expirationTime } = useSelector(selectSession);
+  const { blocks } = useSelector(selectGlobal);
   const isLoggedIn = username && expirationTime > Date.now();
 
   if (!isLoggedIn) {
     return <Navigate to={"/login"} />;
   }
-  return <div>Main</div>;
+  return (
+    <div>
+      <Board blocks={blocks} />
+    </div>
+  );
 }
--- trans-me/src/slices/globalSlice.js
+++ trans-me/src/slices/globalSlice.js
@@ -1,5 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { nanoid } from "nanoid";
 
+// Todo: current initial state is just for testing
 const initialState = {
   // audio operation
   audioFiles: [],
@@ -7,9 +9,31 @@ const initialState = {
   isPlaying: false,
 
   // block operation
-  blocks: [],
+  blocks: [
+    {
+      id: 0,
+      content: "<h1>Hello World</h1>",
+      isHidden: false,
+    },
+    {
+      id: 1,
+      content: "<h2>I am a block!</h2>",
+      isHidden: false,
+    },
+  ],
   isRecording: false,
 };
+
+// const initialState = {
+//   // audio operation
+//   audioFiles: [],
+//   currentPlaying: null,
+//   isPlaying: false,
+
+//   // block operation
+//   blocks: [],
+//   isRecording: false,
+// };
 
 export const globalSlice = createSlice({
   name: "global",
@@ -28,6 +52,68 @@ export const globalSlice = createSlice({
     setBlocks: (state, action) => {
       state.blocks = action.payload;
     },
+    moveBlockUp: (state, action) => {
+      const { id } = action.payload;
+      const index = state.blocks.findIndex((block) => block.id === id);
+      if (index > 0) {
+        // immutable swap
+        let blocksCopy = [...state.blocks];
+        const temp = state.blocks[index - 1];
+        blocksCopy[index - 1] = state.blocks[index];
+        blocksCopy[index] = temp;
+        state.blocks = blocksCopy;
+      }
+    },
+    moveBlockDown: (state, action) => {
+      const { id } = action.payload;
+      const index = state.blocks.findIndex((block) => block.id === id);
+      if (index < state.blocks.length - 1) {
+        const temp = state.blocks[index + 1];
+        state.blocks[index + 1] = state.blocks[index];
+        state.blocks[index] = temp;
+      }
+    },
+    insertBlock: (state, action) => {
+      const { index } = action.payload;
+      const id = nanoid(6);
+      state.blocks = [
+        ...state.blocks.slice(0, index),
+        { id, content: "", isHidden: false },
+        ...state.blocks.slice(index),
+      ];
+    },
+    updateBlockContent: (state, action) => {
+      const { id, content } = action.payload;
+      const index = state.blocks.findIndex((block) => block.id === id);
+      state.blocks[index].content = content;
+    },
+    deleteBlock: (state, action) => {
+      const { id } = action.payload;
+      const index = state.blocks.findIndex((block) => block.id === id);
+      state.blocks = [
+        ...state.blocks.slice(0, index),
+        ...state.blocks.slice(index + 1),
+      ];
+    },
+    duplicateBlock: (state, action) => {
+      const { id } = action.payload;
+      const index = state.blocks.findIndex((block) => block.id === id);
+      const newBlock = {
+        id: nanoid(6),
+        content: state.blocks[index].content,
+        isHidden: false,
+      };
+      state.blocks = [
+        ...state.blocks.slice(0, index + 1),
+        newBlock,
+        ...state.blocks.slice(index + 1),
+      ];
+    },
+    toggleBlockVisibility: (state, action) => {
+      const { id } = action.payload;
+      const index = state.blocks.findIndex((block) => block.id === id);
+      state.blocks[index].isHidden = !state.blocks[index].isHidden;
+    },
     setIsRecording: (state, action) => {
       state.isRecording = action.payload;
     },
@@ -40,6 +126,13 @@ export const {
   setIsPlaying,
   setBlocks,
   setIsRecording,
+  moveBlockUp,
+  moveBlockDown,
+  insertBlock,
+  updateBlockContent,
+  deleteBlock,
+  duplicateBlock,
+  toggleBlockVisibility,
 } = globalSlice.actions;
 
 export const selectGlobal = (state) => state.global;
