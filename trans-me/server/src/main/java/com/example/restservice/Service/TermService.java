fe1b960b6dcc51abf8f676c0d513d0b139be39c4
hansli112 <lixuehan@lixuehandeMacBook-Pro.local>
2022-12-08 17:04:25 +0800

    add term extraction and Google-search link

--- trans-me/server/src/main/java/com/example/restservice/Service/TermService.java
+++ trans-me/server/src/main/java/com/example/restservice/Service/TermService.java
@@ -1,6 +1,9 @@
 package com.example.restservice.Service;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +13,6 @@ import com.example.restservice.Model.Term;
 import com.example.restservice.Repository.TermRepository;
 import com.example.restservice.Response.CommonResponse;
 import com.example.restservice.Response.Msg;
-//import uk.ac.shef.dcs.jate.*;
 
 @Service
 public class TermService {
@@ -19,13 +21,35 @@ public class TermService {
 
     public CommonResponse<List<Term>> handleTerm(String content) {
         List<Term> terms = new LinkedList<>();
-        Term term = new Term("NTU", "National Taiwan University", Arrays.asList("https://www.ntu.edu.tw"));
-        terms.add(term);
 
-        //ContentExtractor contentExtractor = new ContentExtractor();
+        // Extract Terminologies
+        List<String> extractedTerms = extractTerms(content);
+        for (String s: extractedTerms) {
+            Term term = new Term(s, "", Arrays.asList("http://www.google.com/search?q=" + s.replace(' ', '+')));
+            terms.add(term);
+        }
 
         Msg msg = new Msg("success", "get terms");
         return new CommonResponse<List<Term>> (msg, terms);
     }
 
+    private List<String> extractTerms(String content) {
+        List<String> terms = new LinkedList<>();
+
+        try {
+            ProcessBuilder processBuilder = new ProcessBuilder("python", "./src/main/python/com/example/restservice/Service/TermExtract.py", content);
+            Process process = processBuilder.start();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                terms.add(line);
+            }
+        }
+        catch (java.lang.Exception e) {
+            System.out.println(e);
+        }
+        
+        return terms;
+    }
 }
