ef02920bb2f4ea0c9bced7fa7153a4846c0b4cd5
Sebastian <sebastil@hotmail.com>
2022-12-11 00:14:15 +0800

    post and get for audiofiles

--- trans-me/server/src/main/java/com/example/restservice/Controller/AudioController.java
+++ trans-me/server/src/main/java/com/example/restservice/Controller/AudioController.java
@@ -1,5 +1,52 @@
 package com.example.restservice.Controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.restservice.Response.CommonResponse;
+import com.example.restservice.Response.Msg;
+import com.example.restservice.Service.AudioFilesService;
+import com.example.restservice.Service.Payload.Payload;
+import com.example.restservice.Model.AudioFile;
+import com.example.restservice.Request.audioFile.PostAudioFileRequest;
+
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "Audio File Controller")
+@RestController
+@RequestMapping("/audioFile")
 public class AudioController {
-    
+    @Autowired
+    AudioFilesService audioFilesService;
+
+    @GetMapping("")
+    public CommonResponse<List<AudioFile>> getAudioFilesByUsername(@RequestParam("username") String username) {
+        Payload <Msg, List<AudioFile>> result = audioFilesService.getAudioFiles(username);
+
+        return new CommonResponse<List<AudioFile>>(
+            result.getMsg(),
+            result.getData()
+        );
+    }
+
+    @PostMapping("")
+    public CommonResponse postAudioFile(@RequestBody PostAudioFileRequest req) {
+        String username = req.getData().getUsername();
+        String name = req.getData().getName();
+        String format = req.getData().getFormat();
+
+        Payload <Msg, AudioFile> result = audioFilesService.postAudioFile(username, name, format);
+
+        return new CommonResponse <AudioFile>(
+            result.getMsg(),
+            result.getData()
+        );
+    }
 }
