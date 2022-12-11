afcbfe2bd89de51213e4cb9a8d93133b68ddc83c
Sebastian <sebastil@hotmail.com>
2022-12-11 11:26:32 +0800

    post and delete done

--- trans-me/server/src/main/java/com/example/restservice/Service/AudioFilesService.java
+++ trans-me/server/src/main/java/com/example/restservice/Service/AudioFilesService.java
@@ -5,7 +5,10 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.example.restservice.Drive.CreateAudioFile;
+import com.example.restservice.Drive.DeleteFile;
 import com.example.restservice.Model.Account;
 import com.example.restservice.Model.AudioFile;
 import com.example.restservice.Repository.AccountRepository;
@@ -93,7 +96,7 @@ public class AudioFilesService {
         }
     }
 
-    public Payload<Msg, AudioFile> postAudioFile(String username, String name, String format) {
+    public Payload<Msg, AudioFile> postAudioFile(String username, String name, String format, MultipartFile file) {
         Account account = accountRepository.findByUsername(username);
 
         if (account == null) {
@@ -103,13 +106,32 @@ public class AudioFilesService {
                     "error", 
                     "Account not found"
                     ),
-                new AudioFile()
+                null
             );
         }
 
-        System.out.println("Create audio file");
+        String subfolderId = account.getDriveId();
+        List<String> responseGoogleDrive = new ArrayList<String>();
 
-        AudioFile newAudioFile = new AudioFile("", name, "", "", format);
+        System.out.println("Uploading audio file to google drive");
+
+        try {
+            responseGoogleDrive = CreateAudioFile.uploadFile(file, subfolderId);
+        }
+        catch (Exception e) {
+            System.out.println("Error uploading to google drive, " + e);
+            return new Payload<Msg,AudioFile>(
+                new Msg(
+                    "error",
+                    "Error uploading to google drive"
+                ), 
+            null
+            );
+        }
+
+        String driveId = responseGoogleDrive.get(0);
+        String url = responseGoogleDrive.get(1);
+        AudioFile newAudioFile = new AudioFile("", name, url, driveId, format);
 
         AudioFile savedAudioFile = audioFileRepository.save(newAudioFile);
 
@@ -141,5 +163,114 @@ public class AudioFilesService {
                     savedAudioFile
                 );
         }
+    }
+
+    public Payload<Msg, AudioFile> updateAudioFile(String id, String name) {
+        Optional<AudioFile> savedAudioFile = audioFileRepository.findById(id);
+        
+        if (savedAudioFile.isPresent()) {
+            AudioFile updatedAudioFile = savedAudioFile.get();
+            updatedAudioFile.setName(name);
+            AudioFile responseAudioFile = audioFileRepository.save(updatedAudioFile);
+            if (responseAudioFile != null) {
+                System.out.println("Successfully added audio file");
+                return new Payload <Msg, AudioFile> (
+                    new Msg(
+                        "success",
+                        "Successfully added audio file"
+                    ),
+                    responseAudioFile
+                );
+            }
+            else {
+                System.out.println("Error, couldn't add audio file");
+                return new Payload <Msg, AudioFile> (
+                    new Msg(
+                        "error",
+                        "Couldn't add audio file"
+                    ),
+                    responseAudioFile
+                );
+            }
+        }
+        else {
+            System.out.println("Error, couldn't find audio file");
+            return new Payload <Msg, AudioFile> (
+                    new Msg(
+                        "error",
+                        "could not find audio file"
+                        ),
+                    new AudioFile()
+                );
+        }
+    }
+
+    public Payload<Msg, String> deleteAudioFile(String username, String id) {
+        Account account = accountRepository.findByUsername(username);
+        
+        if (account == null) {
+            System.out.println("Account not found");
+            return new Payload<Msg, String>(
+                new Msg(
+                    "error", 
+                    "Account not found"
+                    ),
+                ""
+            );
+        }
+
+        List<String> audioFilesIds = account.getAudioFilesId();
+
+        if (!audioFilesIds.contains(id)) {
+            System.out.println("Audio File doesn't belong to this user");
+            return new Payload<Msg, String>(
+                new Msg(
+                    "error", 
+                    "Audio File doesn't belong to this user"
+                    ),
+                ""
+            );
+        }
+
+        // Remove from account list of audio files
+        audioFilesIds.remove(id);
+        account.setAudioFilesId(audioFilesIds);
+        Account updatedAccount = accountRepository.save(account);
+        System.out.println("Successfully updated account's audio files list: " + updatedAccount.getAudioFilesId());
+
+        Optional<AudioFile> savedAudioFile = audioFileRepository.findById(id);
+        if (!savedAudioFile.isPresent()) {
+            System.out.println("Error, couldn't find audio file in mongoDB");
+            return new Payload <Msg, String> (
+                    new Msg(
+                        "error",
+                        "could not find audio file"
+                        ),
+                    ""
+                );
+        }
+        
+        // Remove from google Drive
+        String googleDriveId = savedAudioFile.get().getDriveId();
+        try {
+            DeleteFile.deleteFile(googleDriveId);
+        }
+        catch (Exception e) {
+            // Might have deleted by mistake, but still will delete from MongoDB
+            System.out.println("Error deleting from google drive");
+        }
+        
+        // Remove from MongoDB    
+        AudioFile audioFileToBeDeleted = savedAudioFile.get();
+        audioFileRepository.delete(audioFileToBeDeleted);
+
+        System.out.println("Successfully deleted audio file");
+        return new Payload<Msg,String>(
+            new Msg(
+                "success",
+                "Successfully deleted audio file"
+            ), 
+            ""
+            );
     }
 }
